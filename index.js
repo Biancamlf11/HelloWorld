9d3594a2dd146b9a22021c830807ba4a64439d1c
Bianca <biancamleguizamonf@gmail.com>
2024-06-17 21:32:08 -0400

    Se prueba primeras paginas

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-console.log("Hola Mundo")
-const numero1=1
-const numero2=2
-console.log(numero1+numero2)
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+app.get('/package', (req, res) => {
+    const numero1 = 1
+    const numero2= 3
+        res.send('La suma es: '+numero1+numero2)
+})
+app.get('/package/express', (req, res) => {
+        res.send('Hola!!')
+})
